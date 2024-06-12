0ad55e5445dea0a8c0c48a234c1b97d62e244518
Chritie MPASSI <christmp19@gmail.com>
2024-06-12 15:54:47 +0100

    feat: Add tailwind css with nativewind

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import './nativewind.config';
+import CustomText from './components/text';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center bg-green-600 justify-center">
+      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <CustomText />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
