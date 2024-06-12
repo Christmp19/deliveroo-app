d19684efd4ea0453d12958659f93d982454acbd9
Christie MPASSI <christmp19@gmail.com>
2024-06-12 17:04:02 +0100

    feat: Add react native navigation

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import './nativewind.config';
-import CustomText from './components/text';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View className="flex-1 items-center bg-green-600 justify-center">
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <CustomText />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
